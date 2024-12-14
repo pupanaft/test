d403f6767dc1a5787e17fbc89bbe95c610e7bdc5
Bogdan_Ishcenko <bogdan.ishchenko.852@gmail.com>
2024-12-14 15:25:50 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  // Входной файл
+  entry: [
+    './src/js/index.js'
+  ],
+
+  // Выходной файл
+  output: {
+    filename: './js/bundle.js'
+  },
+
+  // Source maps для удобства отладки
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      // Транспилируем js с babel
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+
+      // Компилируем SCSS в CSS
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract css to separate file
+          'css-loader', // translates CSS into CommonJS
+           // parse CSS and add vendor prefixes to CSS rules
+          
+             'postcss-loader',
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+
+      // Подключаем шрифты из css
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./fonts/[name].[ext]'
+          },
+        ]
+      },
+
+      // Подключаем картинки из css
+      {
+        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        use: [
+          {
+            loader: 'file-loader?name=./static/[name].[ext]'
+          },
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack 4 Starter',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      }
+    }),
+
+    // Кладем стили в отдельный файлик
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    new CopyPlugin({
+        patterns: [
+            { from: './src/images', to: 'images'},
+        ],
+        options: {
+          concurrency: 100,
+        },
+      }),
+    // Копируем картинки
+   
+  ],
+};
